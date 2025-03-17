8e973644e7bd037e4115c24fb436bbabab588224
inbal-itzhak <itzhak.inbal78@gmail.com>
2025-03-17 15:16:43 +0200

    main

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import allure
 from pages.restaurant_page import RestaurantPage
 from pages.top_menu_page import TopMenuLinksPage
 from pages.search_menu_page import SearchMenuPage
@@ -6,17 +7,35 @@ from pages.search_results_page import SearchResultsPage
 from pages.rest_page_make_reservation import RestOrderReservationPage
 from pages.tabit_order_reservation_page import OrderReservationPage
 import pytest
+from _pytest.fixtures import fixture
 from selenium import webdriver
+
+
 # from selenium.webdriver.chrome.webdriver import WebDriver
+def pytest_exception_interact(node, report):
+    if report.failed:
+        driver = getattr(node.cls, "driver", None)
+        if driver:
+            allure.attach(body=driver.get_screenshot_as_png(), name="failure screenshot",
+                          attachment_type=allure.attachment_type.PNG)
+
+
+# @fixture(scope="class", autouse=True)
+# def driver_init(request):
+#     global driver
+#     driver = webdriver.Chrome()
+#     request.cls.driver = driver
+#     driver.get("https://www.rest.co.il/")
+#     driver.maximize_window()
+#     yield
+#     driver.quit()
 
 
 @pytest.fixture(scope="function", autouse=True)
 def setup_driver_class(request):
     chrome_options = webdriver.ChromeOptions()
-
     # Disable notifications
     chrome_options.add_experimental_option("prefs", {"profile.default_content_setting_values.notifications": 2})
-
     request.cls.driver = webdriver.Chrome(options=chrome_options)
     request.cls.driver.maximize_window()
     request.cls.driver.get("https://www.rest.co.il/")
@@ -30,5 +49,3 @@ def setup_driver_class(request):
     request.cls.top_menu = TopMenuLinksPage(request.cls.driver)
     yield
     request.cls.driver.quit()
-
-
